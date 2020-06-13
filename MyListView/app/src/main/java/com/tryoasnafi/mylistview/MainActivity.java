d6019f713296f7a1268d87e5dab72e94895ffc9a
tryoasnafi <tryoasnafi@gmail.com>
2020-06-13 23:36:51 +0700

    update MainActivity

--- MyListView/app/src/main/java/com/tryoasnafi/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/tryoasnafi/mylistview/MainActivity.java
@@ -3,12 +3,21 @@ package com.tryoasnafi.mylistview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = findViewById(R.id.lv_list);
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        listView.setAdapter(arrayAdapter);
     }
 }
