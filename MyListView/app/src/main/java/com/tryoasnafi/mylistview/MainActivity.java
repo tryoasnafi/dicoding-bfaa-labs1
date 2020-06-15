82c4d6e1550a2c8d5c13f15a897dfb1e4ad477ed
tryoasnafi <tryoasnafi@gmail.com>
2020-06-15 15:37:16 +0700

    update MyListView Hero List

--- MyListView/app/src/main/java/com/tryoasnafi/mylistview/MainActivity.java
+++ MyListView/app/src/main/java/com/tryoasnafi/mylistview/MainActivity.java
@@ -2,13 +2,24 @@ package com.tryoasnafi.mylistview;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.TypedArray;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.tryoasnafi.mylistview.adapter.HeroAdapter;
+import com.tryoasnafi.mylistview.model.Hero;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    public String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+    private HeroAdapter heroAdapter;
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private ArrayList<Hero> heroes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +27,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView listView = findViewById(R.id.lv_list);
-        ArrayAdapter arrayAdapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
-        listView.setAdapter(arrayAdapter);
+        heroAdapter = new HeroAdapter(this);
+        listView.setAdapter(heroAdapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, heroes.get(position).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+
+        heroAdapter.setHeroes(heroes);
     }
 }
---
+++ MyListView/app/src/main/java/com/tryoasnafi/mylistview/adapter/HeroAdapter.java
@@ -0,0 +1,76 @@
+package com.tryoasnafi.mylistview.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.tryoasnafi.mylistview.R;
+import com.tryoasnafi.mylistview.model.Hero;
+
+import java.util.ArrayList;
+
+public class HeroAdapter extends BaseAdapter {
+    private Context ctx;
+    private ArrayList<Hero> heroes = new ArrayList<>();
+
+    // Constructor Context
+    public HeroAdapter(Context ctx) {
+        this.ctx = ctx;
+    }
+
+    // Setter arraylist
+    public void setHeroes(ArrayList<Hero> heroes) {
+        this.heroes = heroes;
+    }
+
+    @Override
+    public int getCount() {
+        return heroes.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return heroes.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View itemView = convertView;
+        if (itemView == null) {
+            itemView = LayoutInflater.from(ctx).inflate(R.layout.item_hero, parent, false);
+        }
+
+        ViewHolder viewHolder = new ViewHolder(itemView);
+
+        Hero hero = (Hero) getItem(position);
+        viewHolder.bind(hero);
+        return itemView;
+    }
+
+    private class ViewHolder {
+        private ImageView imgHero;
+        private TextView tvName;
+        private TextView tvDescription;
+
+        ViewHolder(View view) {
+            imgHero = view.findViewById(R.id.img_hero);
+            tvName = view.findViewById(R.id.tv_name);
+            tvDescription = view.findViewById(R.id.tv_description);
+        }
+
+        void bind(Hero hero){
+            tvName.setText(hero.getName());
+            tvDescription.setText(hero.getDescription());
+            imgHero.setImageResource(hero.getPhoto());
+        }
+    }
+}
